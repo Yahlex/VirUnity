5bc38ff227d2daff837ebf05b7f90648b98596d9
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2026-04-22 12:27:36 +0200

    Adding inventory item check to allow user to finish the game

---
+++ Assets/Code/PlayerInventoryMonoBehavior.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public List<string> items = new List<string>();
+
+    public void AddItem(string itemName)
+    {
+        items.Add(itemName);
+        Debug.Log("Objet ajouté : " + itemName);
+    }
+
+    public bool HasItem(string itemName)
+    {
+        return items.Contains(itemName);
+    }
+}
